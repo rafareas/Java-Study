fcc3732d4f6e1080a5de64d6acd2e1a4c32f4b25
rafael <rafa.areas@hotmail.com>
2023-08-16 14:33:29 +0100

    atualizando exercicio da ficha 2

--- Ficha2/Exercicio1/Ex2Ficha2.java
+++ Ficha2/Exercicio1/Ex2Ficha2.java
@@ -9,11 +9,11 @@ public class Ex2Ficha2 {
         Scanner scanner = new Scanner(System.in);
 
         int[][] notasTurma = {
-                {0,0,0,0,0},
-                {0,0,0,0,0},
-                {0,0,0,0,0},
-                {0,0,0,0,0},
-                {0,0,0,0,0}
+                {0,2,0,0,0},
+                {0,2,0,0,0},
+                {0,2,0,0,0},
+                {0,2,0,10,0},
+                {0,2,0,0,0}
         };
 
         int operacao = 1;
@@ -24,6 +24,8 @@ public class Ex2Ficha2 {
             System.out.println("1 - ler as notas dos alunos");
             System.out.println("2 - actualiza o array da pauta");
             System.out.println("3 - calcular a média das notas de um aluno");
+            System.out.println("4 - calcular a média das notas de uma unidade curricular, dado o índice da unidade curricular");
+            System.out.println("5 - calcular a nota mais alta a todas as unidades curriculares de todos os alunos;");
             System.out.println("0 - exit the program");
             operacao = scanner.nextInt();
 
@@ -38,6 +40,7 @@ public class Ex2Ficha2 {
                     uc = scanner.nextInt();
                     System.out.println("->The note of student "+ numAluno + " in the UC "+uc+" is " + notasTurma[numAluno][uc]+"<-");
                     break;
+
                 case 2:
                     System.out.println("Type the number of the student:");
                     numAluno = scanner.nextInt();
@@ -49,6 +52,7 @@ public class Ex2Ficha2 {
                     notasTurma[numAluno][uc] = note;
                     System.out.println("->the note of student " + numAluno + " in the UC " + uc + " is " + note+ "<-");
                     break;
+
                 case 3:
                     System.out.println("which number of student do you want to calculate the average of notes?");
                     numAluno = scanner.nextInt();
@@ -60,6 +64,42 @@ public class Ex2Ficha2 {
                     double averageNote = totalNotes/5;
 
                     System.out.println("the average notes of student "+numAluno+" is "+averageNote);
+                    break;
+
+                case 4:
+                    System.out.println("type de indexes of UC:");
+                    uc = scanner.nextInt();
+
+                    int totalNotes1 = 0;
+                    for (int i = 0; i < 5; i++){
+                        totalNotes1 += notasTurma[i][uc];
+                    }
+
+                    double averageNote1 = totalNotes1/5;
+
+                    System.out.println("->the average note of UC "+uc+" is "+averageNote1+"<-");
+                    break;
+
+                case 5:
+                    int notas = 0;
+                    int maiorNota = 0;
+                    int numA = 0;
+                    int UC = 0;
+                    for (int i = 0; i < 5;i++){
+                        for (int j = 0; j < 5; j++){
+
+                            notas = notasTurma[i][j];
+
+                            if (notas > maiorNota){
+                                maiorNota = notas;
+                                numA = i;
+                                UC = j;
+                            }
+                        }
+                    }
+
+                    System.out.println("The highest grade is from the student "+numA+" int the UC "+UC+" with grade "+maiorNota);
+
                     break;
                 default:
                     System.out.println("------------the program was finished---------------");
